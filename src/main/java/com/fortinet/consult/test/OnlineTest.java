53a21f4819b6794bdb194601ea0084329654815b
dileepdkumar <89115244+dileepdkumar@users.noreply.github.com>
2022-05-24 19:00:23 +0530

    Update OnlineTest.java

--- src/main/java/com/fortinet/consult/test/OnlineTest.java
+++ src/main/java/com/fortinet/consult/test/OnlineTest.java
@@ -6,7 +6,7 @@ import java.awt.*;
 import java.awt.event.*;  
 import javax.swing.*;  
 
-#import org.apache.log4j.Logger;
+/*import org.apache.log4j.Logger;*/
 
 import java.io.*;
 import java.sql.SQLException;
